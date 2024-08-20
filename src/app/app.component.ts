936cd0bd5a009dac8b91426faf48f6f8da1317f4
Anja Tihić <atihic99@gmail.com>
2024-08-20 14:35:31 +0200

    Create general layout components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ImageViewComponent} from "./image-view/image-view.component";
+import {SidebarComponent} from "./sidebar/sidebar.component";
+import {BottombarComponent} from "./bottombar/bottombar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ImageViewComponent, SidebarComponent, BottombarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
